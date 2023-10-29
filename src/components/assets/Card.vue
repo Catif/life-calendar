0b5e6a984dfa4468f39ba4f1472e80321eb81463
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-10-29 15:52:57 +0100

    feature: new Card component

---
+++ src/components/assets/Card.vue
@@ -0,0 +1,52 @@
+<script setup>
+// ==============
+//    Imports
+// ==============
+
+
+
+// ==============
+//   Components
+// ==============
+
+
+
+// ==============
+//     Store
+// ==============
+
+
+
+// ==============
+//   Variables
+// ==============
+
+
+
+// ==============
+//   Functions
+// ==============
+
+
+
+// ==============
+//     Logic
+// ==============
+</script>
+
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  border-radius: 1rem;
+  overflow: hidden;
+  background-color: hsla(0, 0%, 0%, 0.3);
+  box-shadow: 0px 0px 10px 0px hsla(0, 0%, 0%, 1);
+}
+</style>
