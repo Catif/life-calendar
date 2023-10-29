8cca1cf157eb643cdc189d0aab98e6df9c10bca0
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-10-29 15:54:02 +0100

    feature: create Calendar component

---
+++ src/components/Calendar/Calendar.vue
@@ -0,0 +1,61 @@
+<script setup>
+// ==============
+//    Imports
+// ==============
+import { computed } from 'vue'
+
+
+
+// ==============
+//   Components
+// ==============
+import Year from '../../components/Calendar/Year.vue'
+
+
+
+// ==============
+//     Store
+// ==============
+
+
+
+// ==============
+//   Variables
+// ==============
+const years = computed(() => {
+  const years = []
+  const currentYear = new Date().getFullYear()
+
+  for (let i = currentYear; i >= 2002; i--)
+    years.push(i)
+
+  return years
+})
+
+
+// ==============
+//   Functions
+// ==============
+
+
+
+// ==============
+//     Logic
+// ==============
+</script>
+
+<template>
+  <div class="years">
+    <template v-for="year in years" :key="year">
+      <Year :year-number="year" />
+    </template>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.years {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
+}
+</style>
---
+++ src/components/Calendar/Year.vue
@@ -0,0 +1,68 @@
+<script setup>
+// ==============
+//    Imports
+// ==============
+
+
+
+// ==============
+//   Components
+// ==============
+
+
+
+// ==============
+//     Store
+// ==============
+
+
+
+// ==============
+//   Variables
+// ==============
+const props = defineProps({
+  yearNumber: {
+    type: Number,
+    required: true,
+  },
+})
+
+
+// ==============
+//   Functions
+// ==============
+
+
+
+// ==============
+//     Logic
+// ==============
+</script>
+
+<template>
+  <router-link :to="{ name: 'calendar.year', params: { year: yearNumber } }" class="year">
+    {{ yearNumber }}
+  </router-link>
+</template>
+
+<style lang="scss" scoped>
+.year {
+  color: inherit;
+  text-decoration: none;
+  font-size: 1.5rem;
+  width: 200px;
+  border: 2px solid rgba(255, 255, 255, 0.25);
+  background-color: rgba(255, 255, 255, 0.025);
+  border-radius: 0.25rem;
+  text-align: center;
+
+  &:visited {
+    color: inherit;
+  }
+  &:hover {
+    cursor: pointer;
+    color: inherit;
+
+  }
+}
+</style>
